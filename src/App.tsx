1895217bfd46ee2a0dfd86a814944c7e805f061a
Regin Thangaraj <regin.thangaraj@gds.ey.com>
2020-05-08 20:52:08 +0530

    React app skeleton

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Welcome to Church Website!
+    </div>
+  );
+}
+
+export default App;
