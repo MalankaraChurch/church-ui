2496a2d4d0ab89c72651186a810067bb48e65027
Regin Thangaraj <regin.thangaraj@gds.ey.com>
2020-11-29 20:28:36 +0530

    Added sample youtube videos

--- src/components/home/YouTube/YouTube.tsx
+++ src/components/home/YouTube/YouTube.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import ReactPlayer from "react-player";
 import './YouTube.scss';
 
 type YouTubeProps = {
 };
 
 const YouTube: React.FC<YouTubeProps> = (props: YouTubeProps) => {
-    return (
-      <>
-        YouTube Section
-      </>
-    );
+  return (
+    <div>
+      <ReactPlayer url="https://www.youtube.com/watch?v=CBX4BKUtK1c" />
+      <ReactPlayer url="https://www.youtube.com/watch?v=PxdgfRnRduE" />
+      <ReactPlayer url="https://www.youtube.com/watch?v=l45AStM1q3w" />
+      <ReactPlayer url="https://www.youtube.com/watch?v=GPo8DXjx9R0" />
+    </div>
+  );
 };
 
 export default YouTube;
