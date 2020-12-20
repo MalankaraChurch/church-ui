1e1a8046935fac485f616ea6a804bf034e808689
Regin Thangaraj <regin.thangaraj@gds.ey.com>
2020-12-20 20:29:11 +0530

    Timing content added

--- src/components/home/Timing/Timing.tsx
+++ src/components/home/Timing/Timing.tsx
@@ -7,13 +7,29 @@ type TimingProps = {};
 const Timing: React.FC<TimingProps> = (props: TimingProps) => {
   return (
     <section className="timing">
+      {/* TODO: Different color for section */}
       {/* TODO: Add section heading */}
       <div className="timing__content">
-        5.40 am : Morning Prayer in the Main Shrine<br/>
-        6.00 am : Mass in Tamil in the Main Shrine<br/>
-        7.00 am : Mass in Tamil in the Lower Basilica<br/>
-        8.00 am : Mass in Telugu in the Upper Basilica<br/>
-        9.00 am : Mass in Malayalam in the Lower Basilica<br/>
+        <h3>Holy Mass</h3>
+        <div className="timing__details">
+          <div>All Sunday (7:00 AM - 8:30 AM)</div>
+          <div>All Saturday (12:30 PM - 1:30 PM)</div>
+        </div>
+        <h3>Rosary and Prayers</h3>
+        <div className="timing__details">
+          <div className="timing__item">
+            <div className="timing__name">Rosary</div>
+            <div className="timing__time">Monday to Friday (6:00 PM - 6:45 PM)</div>
+          </div>
+          <div className="timing__item">
+            <div className="timing__name">Navanal Special Prayer</div>
+            <div className="timing__time">Saturday (12:00 PM -12:30 PM)</div>
+          </div>
+          <div className="timing__item">
+            <div className="timing__name">Morning Prayer (Before Holy Mass)</div>
+            <div className="timing__time">Sunday (6:30 AM -7:00 AM)</div>
+          </div>
+        </div>
       </div>
       <div className="timing__image">
         <img src={timingImage} alt="Timing" />
