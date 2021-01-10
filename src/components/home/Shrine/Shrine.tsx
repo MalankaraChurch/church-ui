f54cad4b4d203af1e2e84767d63b3b4231250d1b
Regin Thangaraj <regin.thangaraj@gds.ey.com>
2021-01-10 21:14:28 +0530

    Videos section, and section headings

--- src/components/home/Shrine/Shrine.tsx
+++ src/components/home/Shrine/Shrine.tsx
@@ -1,22 +1,26 @@
-import React from 'react';
+import React from "react";
 import shrineImage from "assets/images/shrine.jpg";
-import './Shrine.scss';
+import "./Shrine.scss";
 
-type ShrineProps = {
-};
+type ShrineProps = {};
 
 const Shrine: React.FC<ShrineProps> = (props: ShrineProps) => {
-    return (
-      <section className="shrine">
-        {/* TODO: Add section heading */}
+  return (
+    <section className="shrine">
+      <h2 className="shrine__heading">Shrine</h2>
+      <div className="shrine__body">
         <div className="shrine__image">
           <img src={shrineImage} alt="Shrine" />
         </div>
         <div className="shrine__content">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto earum accusamus at magnam doloremque consequuntur, reiciendis sunt sit! Et incidunt, hic eveniet blanditiis unde porro iure sit. Consequuntur, quia repellat.
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto earum
+          accusamus at magnam doloremque consequuntur, reiciendis sunt sit! Et
+          incidunt, hic eveniet blanditiis unde porro iure sit. Consequuntur,
+          quia repellat.
         </div>
-      </section>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default Shrine;
--- src/components/home/YouTube/YouTube.tsx
+++ src/components/home/YouTube/YouTube.tsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React from "react";
 import ReactPlayer from "react-player";
-import './YouTube.scss';
+import "./YouTube.scss";
 
-type YouTubeProps = {
-};
+type YouTubeProps = {};
 
 const YouTube: React.FC<YouTubeProps> = (props: YouTubeProps) => {
   return (
-    <div>
-      <ReactPlayer url="https://www.youtube.com/watch?v=CBX4BKUtK1c" />
-      <ReactPlayer url="https://www.youtube.com/watch?v=PxdgfRnRduE" />
-      <ReactPlayer url="https://www.youtube.com/watch?v=l45AStM1q3w" />
-      <ReactPlayer url="https://www.youtube.com/watch?v=GPo8DXjx9R0" />
+    <div className="you-tube">
+      <h2 className="you-tube__heading">Videos</h2>
+      {/* TODO: Prevent video from trimmed. */}
+      <div className="you-tube__body">
+        <div className="you-tube__item">
+          <ReactPlayer url="https://www.youtube.com/watch?v=CBX4BKUtK1c" />
+        </div>
+        <div className="you-tube__item">
+          <ReactPlayer url="https://www.youtube.com/watch?v=PxdgfRnRduE" />
+        </div>
+        <div className="you-tube__item">
+          <ReactPlayer url="https://www.youtube.com/watch?v=l45AStM1q3w" />
+        </div>
+        <div className="you-tube__item">
+          <ReactPlayer url="https://www.youtube.com/watch?v=GPo8DXjx9R0" />
+        </div>
+      </div>
     </div>
   );
 };
