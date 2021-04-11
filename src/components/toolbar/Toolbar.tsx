34a3815d03a08f8615203f86cc6a57f71119294a
Regin Thangaraj <regin.thangaraj@gds.ey.com>
2021-04-11 21:15:33 +0530

    Nav bar style

--- src/components/toolbar/Toolbar.tsx
+++ src/components/toolbar/Toolbar.tsx
@@ -1,37 +1,84 @@
-import React from "react";
-import "./Toolbar.scss";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import "./Toolbar.scss";
+
+enum NavItem {
+  HOME,
+  ACTIVITIES,
+  EVENTS,
+  HISTORY,
+}
 
 type ToolbarProps = {};
 
 const Toolbar: React.FC<ToolbarProps> = (props: ToolbarProps) => {
+  const [activeItem, setActiveItem] = useState<NavItem>(NavItem.HOME);
+
+  const handleNavLinkClick = (item: NavItem) => {
+    setActiveItem(item);
+  };
+
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/" exact>
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/activities" exact>
-              Activities
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/events" exact>
-              Events
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/history" exact>
-              History
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <nav className="navbar">
+      <ul className="navbar__list">
+        <li
+          className={`navbar__list-item ${
+            activeItem === NavItem.HOME ? "navbar__list-item--active" : ""
+          }`}
+        >
+          <NavLink
+            to="/"
+            exact
+            className="navbar__link"
+            onClick={() => handleNavLinkClick(NavItem.HOME)}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li
+          className={`navbar__list-item ${
+            activeItem === NavItem.ACTIVITIES ? "navbar__list-item--active" : ""
+          }`}
+        >
+          <NavLink
+            to="/activities"
+            exact
+            className="navbar__link"
+            onClick={() => handleNavLinkClick(NavItem.ACTIVITIES)}
+          >
+            Activities
+          </NavLink>
+        </li>
+        <li
+          className={`navbar__list-item ${
+            activeItem === NavItem.EVENTS ? "navbar__list-item--active" : ""
+          }`}
+        >
+          <NavLink
+            to="/events"
+            exact
+            className="navbar__link"
+            onClick={() => handleNavLinkClick(NavItem.EVENTS)}
+          >
+            Events
+          </NavLink>
+        </li>
+        <li
+          className={`navbar__list-item ${
+            activeItem === NavItem.HISTORY ? "navbar__list-item--active" : ""
+          }`}
+        >
+          <NavLink
+            to="/history"
+            exact
+            className="navbar__link"
+            onClick={() => handleNavLinkClick(NavItem.HISTORY)}
+          >
+            History
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
